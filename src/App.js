66eb1d79f8928b6ed07a6434e9a199153c354432
Prathamesh Dukare <prathameshdukaremail@gmail.com>
2021-10-27 15:52:04 +0530

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { useState } from 'react'
 import Navbar from './components/Navbar';
-import Input from './components/Input';
+import Home from './components/Home';
 import Footer from './components/Footer';
 import Alert from './components/Alert';
-
+import About from './components/About';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
 	const [alertMsg, setAlertMsg] = useState(null)
@@ -15,12 +16,21 @@ function App() {
 		setAlertMsg(null)
 	}, 2100);
 	return (
-		<div itemID="id">
-			<Navbar appName="Text-Utils" />
-			<Alert message={alertMsg} />
-			<div className="container"><Input textValue="Enter Text Here" callAlert={callAlert} /></div>
-			<Footer />
-		</div>
+		<Router>
+			<div itemID="id">
+				<Navbar appName="Text-Utils" />
+				<Alert message={alertMsg} />
+				<Switch>
+					<Route path="/about">
+						<About />
+					</Route>
+					<Route path="/">
+						<Home textValue="Enter Text Here" callAlert={callAlert} />
+					</Route>
+				</Switch>
+				<Footer />
+			</div>
+		</Router>
 	);
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div className="container text-center about-container">
+            <h1>About Text Utils</h1>
+            <p>Text utils is the text-utility app which can manipulate your text the way you wanted. We have features such as Capitalize , Convert to LowerCase or UpperCase , Remove Extra spaces , etc.</p>
+            <p>This app is made with React as a part of learning process. If you think you can suggest some features or report some bugs please let us know on <a href="mailto:prathameshdukaremail@gmail.com">Mail</a></p>
+            <p>Also this project is open source so you can contribute to it on <a href="https://github.com/prathamesh-dukare">Github</a></p>
+        </div>
+    )
+}
